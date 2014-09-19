078a1070ee13974832f83ab4fae52db5356dd261
Hana Kim <hana.kim@pomona.edu>
2014-09-19 12:16:57 -0700

    OCLint tutorial

--- staticCodeAnalysis/HKViewController.m
+++ staticCodeAnalysis/HKViewController.m
@@ -18,6 +18,22 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // problem code!
+    NSString* temp;
+    NSArray* tempArray;
+    int i=0;
+    switch (i) {
+        case 0:
+            temp=nil;
+        case 1:
+            tempArray = @[temp];
+            NSLog(@"%d",(int)[tempArray objectAtIndex:0]);
+            break;
+        default:
+            break;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
